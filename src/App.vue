89f3b6fa2dcaaf0263c90fb83784223b3aed2e7f
Laurens <laurens.aarnoudse@gmail.com>
2018-10-18 16:42:50 +0200

    Added a submit button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <h1>De nieuwe ✨Vakkenvuller✨</h1>
-    <section id="Faculteit">
-      <p>Onder welke faculteit valt dit vak?</p>
-      <select v-model="selected">
-        <option>{{ selected }}</option>
-        <option>B</option>
-        <option>C</option>
-      </select>
-    </section>
-    <section id="vakNaam">
-      <p>Wat is de naam van het vak?</p>
-      <input v-model="message" placeholder="type hier">
-      <p>Naam: {{ message }}</p>
-    </section>
+    <form id="signup-form" @submit.prevent="postCourse">
+      <section id="Faculteit">
+        <p>Onder welke faculteit valt dit vak?</p>
+        <select v-model="selected">
+          <option>{{ selected }}</option>
+          <option>B</option>
+          <option>C</option>
+        </select>
+      </section>
+      <section id="vakNaam">
+        <p>Wat is de naam van het vak?</p>
+        <input v-model="message" placeholder="type hier">
+        <p>Naam: {{ message }}</p>
+      </section>
+      <button>Sla het vak op</button>
+    </form>
   </div>
 </template>
 
@@ -27,11 +30,16 @@ export default {
       selected: ""
     }
   },
-  created() {
+  created: function() {
     console.log("Page loaded")
     fetch("http://localhost:8000/faculty/")
       .then(response => response.json())
       .then(json => { this.selected = json[0].name[0].value })
+  },
+  methods: {
+    postCourse: function(event){
+      console.log(this.$data.selected)
+    }
   }
 }
 </script>
