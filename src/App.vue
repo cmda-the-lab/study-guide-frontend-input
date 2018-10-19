fb5e93b15104d9f25d54be0067392c77a7861917
Laurens <laurens.aarnoudse@gmail.com>
2018-10-19 12:36:27 +0200

    Added dynamic faculties. This only works if the server passes valid data. If the faculty for instance doesn't have a name object the code will crash. Might have to be some frontend checks?

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,16 @@
     <form id="signup-form" @submit.prevent="postCourse">
       <section id="Faculteit">
         <p>Onder welke faculteit valt dit vak?</p>
-        <select v-model="faculty">
-          <option>{{ faculty }}</option>
-          <option>B</option>
-          <option>C</option>
+        <select>
+          <option v-for="fac in faculty" :key="fac.id">
+            {{ fac.name[lang].value }}
+          </option>
         </select>
       </section>
       <section id="Indicatoren">
         <p>Welke competentie indicatoren zijn vertegenwoordigd/komen terug in dit vak?</p>
         <select >
-          <option v-for="indicator in indicators" :value="indicator.value">
+          <option v-for="indicator in indicators" :key="indicator.id">
             {{ indicator.value }}
           </option>
         </select>
@@ -29,54 +29,64 @@
 </template>
 
 <script>
+//TODO: combine all course data into a course object and have other data like the language setting somewhere else. Either use vuex (store) to implement this
+// or make compononents out of questions and pass them the right data. Or use props.
 const APIUrl = "http://localhost:8000/"
 export default {
   name: "app",
-  data: function(){
+  data: function() {
     return {
+      lang: 0,
       name: "",
       faculty: "",
-      description:"",
-      years:"",
-      learningYears:"",
-      periods:"",
-      credits:"",
-      start:"",
-      end:"",
-      methods:"",
-      methodsSummary:"",
-      coordinators:"",
-      coordinatorsSummary:"",
-      teachers:"",
-      teachersSummary:"",
-      competencies:"",
-      indicators:"",
-      indicatorSummary:"",
-      objectivesSummary:"",
-      program:"",
+      description: "",
+      years: "",
+      learningYears: "",
+      periods: "",
+      credits: "",
+      start: "",
+      end: "",
+      methods: "",
+      methodsSummary: "",
+      coordinators: "",
+      coordinatorsSummary: "",
+      teachers: "",
+      teachersSummary: "",
+      competencies: "",
+      indicators: "",
+      indicatorSummary: "",
+      objectivesSummary: "",
+      program: ""
     }
   },
   created: function() {
     console.log("Page loaded")
-    fetch(APIUrl+"faculty/")
+    fetch(APIUrl + "faculty/")
       .then(response => response.json())
-      .then(json => { this.faculty = json[0].name[0].value })
-    fetch(APIUrl+"indicator/")
+      .then(json => {
+        this.faculty = json
+        console.table(json)
+      })
+    fetch(APIUrl + "indicator/")
       .then(response => response.json())
-      .then(json => { this.indicators = json })
+      .then(json => {
+        this.indicators = json
+      })
   },
   methods: {
-    postCourse: function(){
+    postCourse: function() {
       console.log(this.$data)
-      fetch("http://localhost:8000/course/",{
+      fetch("http://localhost:8000/course/", {
         method: "post",
         headers: {
-            "Content-Type": "application/json; charset=utf-8",
+          "Content-Type": "application/json; charset=utf-8"
         },
         body: JSON.stringify(this.$data)
       })
         .then(response => response.json())
-        .then(json => { console.log(json) })
+        .then(json => {
+          console.log(json)
+        })
     }
   }
 }
