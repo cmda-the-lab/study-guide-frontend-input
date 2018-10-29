4a42cdc891b3666e7cff2390cd23f586c50aea79
Laurens <laurens.aarnoudse@gmail.com>
2018-10-29 16:02:54 +0100

    Fixed relative css issue

--- src/App.vue
+++ src/App.vue
@@ -269,7 +269,7 @@ export default {
   }
 }
 </script>
-<style src="./vue-multiselect/dist/vue-multiselect.min.css">
+<style src="vue-multiselect/dist/vue-multiselect.min.css">
 </style>
 <style>
 #app {
