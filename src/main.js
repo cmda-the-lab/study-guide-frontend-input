d47b5163805a0a8edcb4c0077c496ee4ba560f01
Titus Wormer <tituswormer@gmail.com>
2018-11-06 20:03:34 +0100

    Remove unused dependencies and components

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 import Vue from "vue"
 import App from "./App.vue"
-import DropDown from "./components/drop-down.vue"
-import Multiselect from "vue-multiselect"
-import VModal from "vue-js-modal"
 import "vue-material/dist/vue-material.min.css"
 import "vue-material/dist/theme/default.css"
-import "vue-multiselect/dist/vue-multiselect.min.css"
 import VueMaterial from "vue-material"
 
 Vue.config.productionTip = false
-
-Vue.component("drop-down", DropDown)
-Vue.component("multiselect", Multiselect)
-Vue.use(VModal)
 
 Vue.use(VueMaterial)
 
